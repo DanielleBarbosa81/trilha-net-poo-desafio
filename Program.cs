5fd94ef98aee279952b3cc94911e25103e29231a
Danielle Barbosa <daniellebarbosa.ti81@gmail.com>
2025-09-08 22:01:57 -0300

    Criando um sistema POO para celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Testando Nokia ###");
+            Smartphone nokia = new Nokia("1199999999", "Nokia Tijolão", "1234567890", 32);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine("\n### Testando iPhone ###");
+            Smartphone iphone = new Iphone("2198888888", "iPhone 14", "9876543210", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
